746f13a432108cf81d1a030712e2ab025e5900c3
Taqi Raza <mttalpur@gmail.com>
2020-10-02 23:25:56 +0500

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -66,7 +66,7 @@ winetricks --unattended wmp9 && sleep 5
 echo "disable" > "$WINEPREFIX/.update-timestamp"
 
 # Removing any existing user data
-( cd "$WINEPREFIX/drive_c/" ; rm -rf users ; rm -rf "../windows/temp/*" ) || true
+( cd "$WINEPREFIX/drive_c/" ; rm -rf users ; rm -rf "windows/temp/*" ) || true
 
 # Pre patching dpi setting in WINEPREFIX
 # DPI dword value 240=f0 180=b4 120=78 110=6e 96=60
