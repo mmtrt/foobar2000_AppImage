db16e44d4556933af79868deba3c8214c7bc6a3e
Taqi Raza <mttalpur@gmail.com>
2021-07-02 11:20:12 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -56,6 +56,7 @@ get_wi () {
     VER=$(wget -qO- https://github.com/mmtrt/WINE_AppImage/releases/tag/continuous | grep continuous/ | cut -d '"' -f2 | sed '3s|/| |g' | awk '{print $6}' | sed '/^\s*$/d')
     wget -q https://github.com/mmtrt/WINE_AppImage/releases/download/continuous/"${VER}" -P ./test ; chmod +x ./test/"$VER" ; ls -al ./test
     sudo ln -s ./test/"$VER" /usr/bin/wine ; sudo ln -s ./test/"$VER" /usr/bin/wineboot ; sudo ln -s ./test/"$VER" /usr/bin/wineserver ; sudo ln -s ./test/"$VER" /usr/bin/winetricks ; ls -al /usr/bin | grep wine
+    export PATH=$PATH:/usr/bin
 }
 
 f2kswp () {
