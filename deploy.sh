8ebd8894ccd0ac80f1ea10720dd9d8033cca28d5
Taqi Raza <mttalpur@gmail.com>
2022-09-01 00:49:17 +0500

    k2

--- deploy.sh
+++ deploy.sh
@@ -312,7 +312,9 @@ chmod +x *.AppImage ; mv wine-stable-amd64_4.0.4-x86_64.AppImage wine-stable.App
 ls -al
 
 # Create WINEPREFIX
-./wine-stable.AppImage wineboot ; sleep 5
+./wine-stable.AppImage --appimage-extract
+( cd "squashfs-root/opt/wine-stable/bin/" ; ln -s wine64 wine ) || true
+./squashfs-root/AppRun wineboot ; sleep 5
 
 ls -al $WINEPREFIX/drive_c/
 
