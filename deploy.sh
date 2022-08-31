7b2a74353cb08f56930b4b1ff35180580afdbc57
Taqi Raza <mttalpur@gmail.com>
2022-08-31 23:51:44 +0500

    test

--- deploy.sh
+++ deploy.sh
@@ -133,7 +133,27 @@ export WINEARCH="win32"
 export WINEPREFIX="/home/runner/work/foobar2000_AppImage/foobar2000_AppImage/AppDir/winedata/.wine"
 export WINEDEBUG="-all"
 
-f2ks ; rm ./*AppImage* $WINEPREFIX
+# Download icon:
+wget -q https://github.com/mmtrt/foobar2000/raw/master/snap/local/src/foobar2000.png
+
+VER=$(wget -qO- https://github.com/AppImageCrafters/appimage-builder/releases/tag/v1.1.0 | grep x86_64 | cut -d'"' -f2 | head -1)
+wget -q https://github.com"${VER}" -O builder ; chmod +x builder
+
+# f2k stable
+stable_ver=$(wget http://www.foobar2000.org/download -q -S -O - 2>&1 | grep foobar2000_v | awk '{print $4}'|sed '2,3d;s|v||;s|</a><br/>||;s|</a>||')
+wget -q https://www.foobar2000.org/download -nH --cut-dirs=3 -r -l 2 -A exe -R '*beta*.exe' ; rm *x64*.exe
+wget -q https://www.foobar2000.org/encoderpack -nH --cut-dirs=3 -r -l 2 -A exe
+7z x "foobar2000_v*.exe" -x'!$PLUGINSDIR' -x'!$R0' -x'!foobar2000 Shell Associations Updater.exe' -x'!uninstall.exe' -o"f2k-stable/usr/share/foobar2000" &>/dev/null
+7z x "Free_*.exe" -x'!$PLUGINSDIR' -o"f2k-stable/usr/share/foobar2000/encoders" &> /dev/null
+find "f2k-stable/usr" -type d -execdir chmod 755 {} +
+touch f2k-stable/usr/share/foobar2000/portable_mode_enabled
+rm *.exe
+
+cp foobar2000.desktop f2k-stable ; cp wrapper f2k-stable ; sed -i -e 's|progVer=|progVer='"$stable_ver"'|g' f2k-stable/wrapper
+
+mkdir -p f2k-stable/usr/share/icons ; cp foobar2000.png f2k-stable/usr/share/icons
+
+mkdir -p AppDir/winedata ; cp -r "f2k-stable/"* AppDir
 
 wget -q https://github.com/mmtrt/WINE_AppImage/releases/download/continuous-stable-4-i386/wine-stable-i386_4.0.4-i686.AppImage
 chmod +x *.AppImage ; mv wine-stable-i386_4.0.4-i686.AppImage wine-stable.AppImage
@@ -159,7 +179,27 @@ export WINEARCH="win64"
 export WINEPREFIX="/home/runner/work/foobar2000_AppImage/foobar2000_AppImage/AppDir/winedata/.wine"
 export WINEDEBUG="-all"
 
-f2ks64 ; rm ./*AppImage* $WINEPREFIX
+# Download icon:
+wget -q https://github.com/mmtrt/foobar2000/raw/master/snap/local/src/foobar2000.png
+
+VER=$(wget -qO- https://github.com/AppImageCrafters/appimage-builder/releases/tag/v1.1.0 | grep x86_64 | cut -d'"' -f2 | head -1)
+wget -q https://github.com"${VER}" -O builder ; chmod +x builder
+
+# f2k stable
+stable_ver=$(wget http://www.foobar2000.org/download -q -S -O - 2>&1 | grep foobar2000_v | awk '{print $4}'|sed '2,3d;s|v||;s|</a><br/>||;s|</a>||')
+wget -q https://www.foobar2000.org/download -nH --cut-dirs=3 -r -l 2 -A exe -R '*beta*.exe' ; rm *0_*.exe
+wget -q https://www.foobar2000.org/encoderpack -nH --cut-dirs=3 -r -l 2 -A exe
+7z x "foobar2000-*_*.exe" -x'!$PLUGINSDIR' -x'!$R0' -x'!foobar2000 Shell Associations Updater.exe' -x'!uninstall.exe' -o"f2k-stable/usr/share/foobar2000" &>/dev/null
+7z x "Free_*.exe" -x'!$PLUGINSDIR' -o"f2k-stable/usr/share/foobar2000/encoders" &> /dev/null
+find "f2k-stable/usr" -type d -execdir chmod 755 {} +
+touch f2k-stable/usr/share/foobar2000/portable_mode_enabled
+rm *.exe
+
+cp foobar2000.desktop f2k-stable ; cp wrapper f2k-stable ; sed -i -e 's|progVer=|progVer='"$stable_ver"'|g' f2k-stable/wrapper
+
+mkdir -p f2k-stable/usr/share/icons ; cp foobar2000.png f2k-stable/usr/share/icons
+
+mkdir -p AppDir/winedata ; cp -r "f2k-stable/"* AppDir
 
 wget -q https://github.com/mmtrt/WINE_AppImage/releases/download/continuous-stable-4-amd64/wine-stable-amd64_4.0.4-x86_64.AppImage
 chmod +x *.AppImage ; mv wine-stable-amd64_4.0.4-x86_64.AppImage wine-stable.AppImage
@@ -190,7 +230,27 @@ export WINEARCH="win32"
 export WINEPREFIX="/home/runner/work/foobar2000_AppImage/foobar2000_AppImage/AppDir/winedata/.wine"
 export WINEDEBUG="-all"
 
-f2kb ; rm ./*AppImage*
+# Download icon:
+wget -q https://github.com/mmtrt/foobar2000/raw/master/snap/local/src/foobar2000.png
+
+VER=$(wget -qO- https://github.com/AppImageCrafters/appimage-builder/releases/tag/v1.1.0 | grep x86_64 | cut -d'"' -f2 | head -1)
+wget -q https://github.com"${VER}" -O builder ; chmod +x builder
+
+beta_ver=$(wget http://www.foobar2000.org/download -q -S -O - 2>&1 | grep getfile | tail -n1 | sed 's|v| |;s| b|-b|' | awk '{print $3 $4}')
+wget -q --accept "*beta*.exe" https://www.foobar2000.org/download -nH --cut-dirs=3 -r -l 2
+wget -q https://www.foobar2000.org/encoderpack -nH --cut-dirs=3 -r -l 2 -A exe ; rm *x64*.exe
+7z x "foobar2000_v*.exe" -x'!$PLUGINSDIR' -x'!$R0' -x'!foobar2000 Shell Associations Updater.exe' -x'!uninstall.exe' -o"f2k-beta/usr/share/foobar2000" &>/dev/null
+7z x "Free_*.exe" -x'!$PLUGINSDIR' -o"f2k-beta/usr/share/foobar2000/encoders" &> /dev/null
+find "f2k-beta/usr" -type d -execdir chmod 755 {} +
+touch f2k-beta/usr/share/foobar2000/portable_mode_enabled
+rm *.exe
+
+cp foobar2000.desktop f2k-beta ; cp wrapper f2k-beta ;
+sed -i -e 's|progVer=|progVer='"$beta_ver"'|g' f2k-beta/wrapper
+
+mkdir -p f2k-beta/usr/share/icons ; cp foobar2000.png f2k-beta/usr/share/icons
+
+mkdir -p AppDir/winedata ; cp -r "f2k-beta/"* AppDir
 
 wget -q https://github.com/mmtrt/WINE_AppImage/releases/download/continuous-stable-4-i386/wine-stable-i386_4.0.4-i686.AppImage
 chmod +x *.AppImage ; mv wine-stable-i386_4.0.4-i686.AppImage wine-stable.AppImage
@@ -224,7 +284,27 @@ export WINEARCH="win64"
 export WINEPREFIX="/home/runner/work/foobar2000_AppImage/foobar2000_AppImage/AppDir/winedata/.wine"
 export WINEDEBUG="-all"
 
-f2kb64 ; rm ./*AppImage*
+# Download icon:
+wget -q https://github.com/mmtrt/foobar2000/raw/master/snap/local/src/foobar2000.png
+
+VER=$(wget -qO- https://github.com/AppImageCrafters/appimage-builder/releases/tag/v1.1.0 | grep x86_64 | cut -d'"' -f2 | head -1)
+wget -q https://github.com"${VER}" -O builder ; chmod +x builder
+
+beta_ver=$(wget http://www.foobar2000.org/download -q -S -O - 2>&1 | grep getfile | tail -n1 | sed 's|v| |;s| b|-b|' | awk '{print $3 $4}')
+wget -q --accept "*beta*.exe" https://www.foobar2000.org/download -nH --cut-dirs=3 -r -l 2
+wget -q https://www.foobar2000.org/encoderpack -nH --cut-dirs=3 -r -l 2 -A exe ; rm *0_*.exe
+7z x "foobar2000-*_*.exe" -x'!$PLUGINSDIR' -x'!$R0' -x'!foobar2000 Shell Associations Updater.exe' -x'!uninstall.exe' -o"f2k-beta/usr/share/foobar2000" &>/dev/null
+7z x "Free_*.exe" -x'!$PLUGINSDIR' -o"f2k-beta/usr/share/foobar2000/encoders" &> /dev/null
+find "f2k-beta/usr" -type d -execdir chmod 755 {} +
+touch f2k-beta/usr/share/foobar2000/portable_mode_enabled
+rm *.exe
+
+cp foobar2000.desktop f2k-beta ; cp wrapper f2k-beta ;
+sed -i -e 's|progVer=|progVer='"$beta_ver"'|g' f2k-beta/wrapper
+
+mkdir -p f2k-beta/usr/share/icons ; cp foobar2000.png f2k-beta/usr/share/icons
+
+mkdir -p AppDir/winedata ; cp -r "f2k-beta/"* AppDir
 
 wget -q https://github.com/mmtrt/WINE_AppImage/releases/download/continuous-stable-4-amd64/wine-stable-amd64_4.0.4-x86_64.AppImage
 chmod +x *.AppImage ; mv wine-stable-amd64_4.0.4-x86_64.AppImage wine-stable.AppImage
