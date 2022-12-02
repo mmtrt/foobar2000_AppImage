c2aaca1c115491ba287309134d1e02d2201d32b4
Taqi Raza <mttalpur@gmail.com>
2022-12-02 17:19:20 +0500

    Create aarch64 version appaimage

--- deploy.sh
+++ deploy.sh
@@ -155,6 +155,33 @@ sed -i 's/stable64|/stable64-wp|/' f2k-x64.yml
 
 }
 
+f2ks-box86 () {
+
+# Download icon:
+wget -q https://github.com/mmtrt/foobar2000/raw/master/snap/local/src/foobar2000.png
+
+wget -q "https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.0.3/appimage-builder-1.0.3-x86_64.AppImage" -O builder ; chmod +x builder
+
+# f2k stable
+stable_ver=$(wget http://www.foobar2000.org/download -q -S -O - 2>&1 | grep foobar2000_v | awk '{print $4}'|sed '2,3d;s|v||;s|</a><br/>||;s|</a>||')
+wget -q https://www.foobar2000.org/download -nH --cut-dirs=3 -r -l 2 -A exe -R '*beta*.exe' ; rm *x64*.exe
+wget -q https://www.foobar2000.org/encoderpack -nH --cut-dirs=3 -r -l 2 -A exe
+7z x "foobar2000_v*.exe" -x'!$PLUGINSDIR' -x'!$R0' -x'!foobar2000 Shell Associations Updater.exe' -x'!uninstall.exe' -o"f2k-stable/usr/share/foobar2000" &>/dev/null
+7z x "Free_*.exe" -x'!$PLUGINSDIR' -o"f2k-stable/usr/share/foobar2000/encoders" &> /dev/null
+find "f2k-stable/usr" -type d -execdir chmod 755 {} +
+touch f2k-stable/usr/share/foobar2000/portable_mode_enabled
+rm *.exe
+
+cp foobar2000.desktop f2k-stable ; cp wrapper f2k-stable ; sed -i -e 's|progVer=|progVer='"$stable_ver"'|g' f2k-stable/wrapper
+
+mkdir -p f2k-stable/usr/share/icons ; cp foobar2000.png f2k-stable/usr/share/icons
+
+mkdir -p AppDir/winedata ; cp -r "f2k-stable/"* AppDir
+
+./builder --recipe f2k-box86.yml
+
+}
+
 if [ "$1" == "stable" ]; then
     f2ks
     ( mkdir -p dist ; mv foobar2000*.AppImage* dist/. ; cd dist || exit ; chmod +x ./*.AppImage )
@@ -166,5 +193,8 @@ elif [ "$1" == "stablewp" ]; then
     ( mkdir -p dist ; mv foobar2000*.AppImage* dist/. ; cd dist || exit ; chmod +x ./*.AppImage )
 elif [ "$1" == "stablewp64" ]; then
     f2kswp64
+    ( mkdir -p dist ; mv foobar2000*.AppImage* dist/. ; cd dist || exit ; chmod +x ./*.AppImage )
+elif [ "$1" == "stable-box86" ]; then
+    f2ks-box86
     ( mkdir -p dist ; mv foobar2000*.AppImage* dist/. ; cd dist || exit ; chmod +x ./*.AppImage )
 fi
